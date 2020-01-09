08fccf1b1bc2f4f96b28ad6df13a0a31b78196ef
Suelio Sousa <suelio@rav.eti.br>
2020-01-09 10:46:05 -0300

    Create Order

--- functions/Order/CreateOrder/lambda_function.py
+++ functions/Order/CreateOrder/lambda_function.py
@@ -1,11 +1,19 @@
 import boto3
 import json
 import uuid
+import decimal
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return float(o)
+        return super(DecimalEncoder, self).default(o)
 
 def lambda_handler(event, context):
     order = event["body"]
     order["id"] = str(uuid.uuid4())
     order["sort_key"] = 'orders'
+    order["price"] = json.dumps(decimal.Decimal(order["price"]), cls=DecimalEncoder)
 
     params = {
         'method': 'create',
