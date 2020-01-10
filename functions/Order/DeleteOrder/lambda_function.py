12fda1c4653522cd12072503aac7b47a2f971653
Suelio Sousa <suelio@rav.eti.br>
2020-01-10 19:13:50 -0300

    Delete Order

---
+++ functions/Order/DeleteOrder/lambda_function.py
@@ -0,0 +1,22 @@
+import boto3
+
+def lambda_handler(event, context):
+    params = {
+        'method': 'delete',
+        'params': {
+            'id': event["order_id"],
+            'table': 'orders',
+            'sort_key': 'orders'
+        }
+    }
+    response = call_function("burger-api-dev", "GenericDao", params)
+    return response
+
+def call_function(Namespace, FunctionName, Params):
+    callable_function = "{}-{}".format(Namespace, FunctionName)
+    client = boto3.client('lambda')
+    responseStream = client.invoke(FunctionName=callable_function, InvocationType='RequestResponse', Payload=json.dumps(Params))
+    payload = responseStream['Payload']
+    responseString = payload.read()
+    response = json.loads(responseString)
+    return response
